ec9e377c75d3b63dd6e66ad2927b722f7a221abc
John Brooke <johnabrooke@gmail.com>
2021-04-24 22:55:51 -0600

    prove01 assignment files

---
+++ prove01-routes.js
@@ -0,0 +1,49 @@
+
+
+const requestBus = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<html>`);
+        res.write(`<head><title>Enter Username</title></head>`);
+        res.write(`<body>
+                    <h1>Hello there!</h1>
+                    <form action="/create-user" method="POST"><label for="username">Username </label><input type="text" name="username"></input> <button type="submit">Submit</button></form>
+                </body>`);
+        res.write(`</html>`);
+        return res.end();
+    }
+    if (url === '/users') {
+        res.write(`<html>
+                    <head><title>List of Users</title></head>
+                    <body>
+                        <ul>
+                            <li>Joe</li>
+                            <li>Havica</li>
+                            <li>Supero</li>
+                            <li>Awecool</li>
+                            <script>parentNode.append(username);</script>
+                        </ul>
+                    </body>
+                </html>`);
+        return res.end();
+    }
+    if (url === '/create-user' && method === 'POST') {
+        const body = [];
+        req.on('data', (info) => {
+            console.log(info);
+            body.push(info);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const username = parsedBody.split('=') [1];
+            console.log(username);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/users');
+        res.end();
+    }
+}
+
+exports.handler = requestBus;
